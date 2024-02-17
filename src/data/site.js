469ed5c54320c1c0788e95c6888afa278acbd298
star8in <sergeevem2007@gmail.com>
2024-02-17 11:58:14 +0400

    feat: add section points

--- src/data/site.js
+++ src/data/site.js
@@ -1,3 +1,11 @@
 export default {
 	path: "/src/data/site.js",
+	points: [
+		{ img: "/src/assets/img/points/1.png", text:"Текст <span class=\"text-primary\">Текст текст</span> Текст текст Текст"},
+		{ img: "/src/assets/img/points/2.png", text:"Текст Текст Текст Текст Текст </br>Текст Текст </br>Текст Текст Текст"},
+		{ img: "/src/assets/img/points/3.png", text:"Текст Текст Текст </br>Текст"},
+		{ img: "/src/assets/img/points/4.png", text:"Текст Текст Текст Текст </br>Текст Текст </br>Текст Текст"},
+		{ img: "/src/assets/img/points/5.png", text:"<span class=\"text-primary\">Текст Текст Текст</span>, </br>Текст Текст Текст Текст Текст Текст"},
+		{ img: "/src/assets/img/points/6.png", text:"Текст </br>Текст  Текст  Текст </br>Текст Текст"}
+	]
 };
